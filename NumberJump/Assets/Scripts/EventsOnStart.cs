2f2be70aff7965e13c6e5b719ac742d9c56d5dbc
Nino <ninoplane@gmail.com>
2023-01-04 20:57:59 +0100

    Working door and keypad

--- NumberJump/Assets/Scripts/EventsOnStart.cs
+++ NumberJump/Assets/Scripts/EventsOnStart.cs
@@ -7,17 +7,16 @@ using TMPro;
 public class EventsOnStart : MonoBehaviour
 {
     public static bool IsStarted = false;
+    public static int random;
     public GameObject textNumberField;
     public GameObject cameraFollow;
     public GameObject buttonMenuUI;
-
     TextMeshProUGUI textNumberField_text;
 
     // Start is called before the first frame update
     void Start()
     {
-        int random = Random.Range(10000, 99999);
-        PlayerPrefs.SetInt("code", random);
+        random = Random.Range(10000, 99999);
         textNumberField_text = textNumberField.GetComponent<TextMeshProUGUI>();
         textNumberField_text.text = random.ToString();
         cameraFollow.SetActive(false);
