3aea6f2c58178f95f23f3c9f6c0ebedadd7833ed
Nino <ninoplane@gmail.com>
2023-01-05 22:01:47 +0100

    End of level 2 working and end screen

--- NumberJump/Assets/Scripts/EndOfLevel.cs
+++ NumberJump/Assets/Scripts/EndOfLevel.cs
@@ -5,15 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class EndOfLevel : MonoBehaviour
 {
+    public int levelToLoad;
     public GameObject finishLineCollider;
+    public GameObject endScreen;
     public GameObject playerCapsule;
+    public GameObject cameraFollow;
 
     // Update is called once per frame
     void Update()
     {
       if(finishLineCollider.GetComponent<Collider>().bounds.Intersects(playerCapsule.GetComponent<Collider>().bounds))
       {
-        SceneManager.LoadScene("Level2");
+        if(levelToLoad == 1)
+        {
+          SceneManager.LoadScene("Level2");
+        }
+        else if(levelToLoad == 2)
+        {
+        Cursor.lockState = CursorLockMode.None;
+        playerCapsule.SetActive(false);
+        cameraFollow.SetActive(false);
+          StartCoroutine(loadEndScreen());
+        }
       }
-    }
+  }
+
+  IEnumerator loadEndScreen()
+  {
+
+    endScreen.SetActive(true);
+    yield return new WaitForSeconds(5);
+    SceneManager.LoadScene("MainMenu");
+  }
 }
